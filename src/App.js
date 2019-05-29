bc3f88b54be8b6cedafd879e463e775203894cc1
SovietSpy2 <sovietspy2@yandex.com>
2019-05-29 20:00:48 +0200

    almost got

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Switch from "react-switch";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+      switchOne: true,
+      switchTwo: false,
+      switchThree: true,
+  }
+  
+  render() {
+    return (
+      <div className="container">
+      
+      <div>
+      <label>
+          <span>Social life</span>
+          <Switch onChange={(v) => this.handleChange("switchOne",v)} checked={this.state.switchOne} />
+      </label>
+      </div>
+      <div>
+      <label>
+          <span>Work</span>
+          <Switch onChange={(v) => this.handleChange("switchTwo",v)} checked={this.state.switchTwo} />
+      </label>
+      </div>
+      <div>
+      <label>
+          <span>Sleep</span>
+          <Switch onChange={(v) => this.handleChange("switchThree",v)} checked={this.state.switchThree} />
+      </label>
+      </div>
+        
+      </div>
+    );
+  }
+
+  handleChange(key,v) {
+
+    const switches = ["switchOne", "switchTwo", "switchThree"].filter(x => x !== key);
+
+    console.log(switches);
+
+    this.setState({
+      [key]: v,
+      [switches[0]]: this.state[switches[0]],
+      [switches[0]]: !this.state[switches[0]]
+      });
+    }
+ 
 }
 
 export default App;
